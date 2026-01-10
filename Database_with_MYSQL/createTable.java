27a01a4a8106904f26bbc2e61a33b886cda028f6
Shiba Bhatta <sibabhatta258@gmail.com>
2026-01-10 06:10:31 +0545

    Refactor createTable.java by removing comments
    Removed unnecessary comments and author tag from createTable.java.

--- Database_with_MYSQL/createTable.java
+++ Database_with_MYSQL/createTable.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database.with.mysql;
 
-/**
- *
- * @author Lenovo
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
